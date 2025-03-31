ceffffe6abcaf26ff1d6a21644ac8bac4e5d31b5
maglezdoval <maglez.doval@gmail.com>
2025-03-31 19:43:18 +0200

    Update gastos_familiares_app.py

--- gastos_familiares_app.py
+++ gastos_familiares_app.py
@@ -6,19 +6,12 @@ import seaborn as sns
 def asignar_categoria(row, categorias):
     descripcion = row['CONCEPTO'].lower()
     comercio = str(row['COMERCIO']).lower()
+    categoria_manual = row.get('CATEGORÍA', None)  # Intenta obtener la categoría del archivo
 
-    # Priorizar la categoría y subcategoría si están definidas
-    if not pd.isna(row['CATEGORÍA']) and row['CATEGORÍA'] != '':
-        return row['CATEGORÍA']  # Usa la categoría existente
-
-    # Buscar en la descripción
-    for categoria, palabras_clave in categorias.items():
-        for palabra in palabras_clave:
-            if palabra in descripcion or palabra in comercio:
-                return categoria
+    if not pd.isna(categoria_manual) and categoria_manual != '':
+        return categoria_manual
 
     return 'Otros'  # Si no coincide con ninguna palabra clave
-
 
 def main():
     st.title('Gestor de Gastos Familiares')
@@ -30,61 +23,36 @@ def main():
         try:
             df = pd.read_csv(uploaded_file, sep=';')  # Usar el separador ';'
 
-            # 2. Crear la columna 'Fecha'
+            # 2. Convertir la columna 'Fecha'
             df['Fecha'] = pd.to_datetime(df['AÑO'].astype(str) + '-' + df['MES'].astype(str) + '-' + df['DIA'].astype(str))
 
             # 3. Convertir la columna 'IMPORTE' a numérico
             df['Importe'] = df['IMPORTE'].str.replace(',', '.').astype(float)
 
-            # 4. Definir las categorías (puedes permitir al usuario editar esto)
-            categorias = {
-                'Alimentación': ['supermercado', 'carrefour', 'dia', 'alcampo', 'mercadona', 'hipercor', 'supercor', 'eroski', 'alimentación', 'fripesa', 'premium meat', 'varietal club', 'levadura madre', 'super yang','barrado', 'almacen frutas','el escudero','delicias manche','coop san isidro', 'M. Consuelo Sae','explaspedronera','obrador de javi','manacor','charcuteria tom','market las roza','24 horas la ria', 'tienda 24 horas','barrado', 'jamoncito','supermercado sp'],
-                'Casa': ['alquiler', 'hipoteca', 'luz', 'agua', 'gas', 'internet', 'canal de isabel ii', 'iberdrola', 'podo', 'comunidad', 'canal de isabel','geo alternativa'],
-                'Ocio': ['restaurante', 'cine', 'bar', 'netflix', 'spotify', 'teatro', 'concierto', 'vaca nostra', 'cervezas la vir', 'diver karting', 'estanco', 'copas', 'tapear', 'birras', 'starbucks', 'circo price', 'parque warner', 'bolera','maquinas vendin', 'pub', 'copas', 'el 17 moreto', 'the loft','kiosko','tagomago playa','tagomago beach','cafes mora', 'café', 'vending','terrraza de ming', 'arroz', 'arroz', 'bar isidro','bar las cadenas', 'boho market', 'chiringuito la', 'escondite de', 'atracciones luq', 'ilunion tartess', 'atracciones luq', 'market tartessu','vaca nostra','one hundred restrooms','one hundred restroom','cedipsa oficina','area comedor gr', 'circo price bar','el patio', 'area s. la gran', 'el patio','cedipsa e.s. mo','el 17 moreto','area s. la gran',' nyx*nordissocialconsul', 'alianza vendin','kiosko c/ kalam','est madrid atoc','serveo servicio', 'el escondite de', 'bar lenito', 'lacaile','parque guerra', 'la fuentona', 'parque warner', 'cinesa',Onneca Restaura','La Posada De Lo','Almagro Oro','lateral caleido','cabildo metropo','museo del foro','Secrets Caleido','Tacos Don Manol','The Loft', 'area s. La gran', 'El Patio'],
-                'Transporte': ['gasolina', 'autobus', 'taxi', 'metro', 'carburante', 'cabify', 'autopista', 'autobus','coche', 'parking', 'cepsa hq madrid','impulsa eventos', 'auto pista', 'peaje', 'atpseitt aeropuert','autopista ap-36', 'autopista r4 se','cepsa hq madrid'],
-                'Salud': ['farmacia', 'medico', 'dentista', 'hospital', 'farmacias', 'podologo','Farmacia Parque', 'Farmacia Gil Al', 'clinica narvart', 'servicios medicos','farmacia el bur', 'farmacia nativi', 'farmacia sada', 'clinica cemtro','farmacia','clinica narvart','Lcda Maria Paz Garcia','Farmacia Carlavilla','Farmaciachicano','Clínica Ojeda'],
-                'Ropa': ['zara', 'h&m', 'c&a', 'decathlon', 'cortefiel', 'bershka', 'stradivarius','puma europe gmbh','converse 8111','coolmania', 'tienda gran pla','tienda 24 horas','mercado persa', 'sigler', 'market tartessu','nike', 'frikinh','lotería'],
-                'Suministros': ['iberdrola smart', 'endesa', 'smart energy', 'suministros','iberdrola smart','suministros', 'elcogas','mercado libre','plenitude', 'podo', 'canal de isabel ii','geo alternativa'],
-                'Otros': ['gastos varios', 'cajero automatico','diversión','Bizum', 'Sin Concepto',' Bizum Apth'],
-                'Donaciones': ['aecc', 'cruz roja', 'fundacion corazon', 'donaciones', 'asociacion española contra el cancer', 'española contra el cancer','fundación corazón','fundacion española del corazon'],
-                'Transferencia': ['transferencia', 'traspaso','CUENTA FAMILIAR', 'CUENTA COMÚN','cuenta familiar'],
-                'Suscripciones': ['apple.com', 'netflix', 'hbo max', 'spotify', 'prime video', 'sky showtime', 'disney plus', 'wordpress','amazon prime','duolingo','telefónica','filmin','rakuten tv','spotify','juegos','tidal'],
-                'Comisiones': ['comisiones','Santanderpremia','liquidación','intereses','tarjeta','comisión'],
-                'Comunidad':['comunidad', 'propietarios','cp. modesto lafuente,','c. p. modesto lafuente'],
-                'Supermercado':['mercadona','carrefour','dia','Simply'],
-                'Cuidado Personal':['Alexso', 'Tijeritas Magic','Peluquería'],
-                'Movistar':['Telefonica', 'Movistar'],
-                'Limpieza': ['Sabbahi Meziou','Noufisa sabban meziane', 'Mina'],
-                'Electrodomésticos':['Wizink', 'Financiera El Corte Inglés' ],
-                'Transporte': ['Cabify'],
-                'Alquiler': ['Fontanería']
-            }
+            #4. Asignar la columna categoría
+            df['Categoria'] = df.apply(asignar_categoria, axis=1, categorias={}) # Inicia vacío
 
-            # 5. Asignar categoría
-            df['Categoria'] = df.apply(lambda row: asignar_categoria(row, categorias), axis=1)
-
-            # 6. Crear una columna 'Tipo' basada en si el importe es positivo o negativo
-            df['Importe'] = df['IMPORTE'].str.replace(',', '.').astype(float)
+            # 5. Crear una columna 'Tipo' basada en si el importe es positivo o negativo
             df['Tipo'] = df['Importe'].apply(lambda x: 'Ingreso' if x > 0 else 'Gasto')
 
-            # 7. Imprimir las categorías únicas encontradas
+            # 6. Imprimir las categorías únicas encontradas
             print(df['Categoria'].unique())
 
-            # 8. Mostrar datos
+            # 7. Mostrar datos
             st.subheader('Transacciones')
             st.dataframe(df)
 
-            # 9. Resumen de gastos por categoría
+            # 8. Resumen de gastos por categoría
             st.subheader('Gastos por Categoría')
             gastos_por_categoria = df.groupby('Categoria')['Importe'].sum().sort_values(ascending=False)
             st.bar_chart(gastos_por_categoria)
 
-            # 10. Resumen de ingresos por categoría
+            # 9. Resumen de ingresos por categoría
             st.subheader('Ingresos por Categoría')
             ingresos_por_categoria = df[df['Tipo'] == 'Ingreso'].groupby('Categoria')['Importe'].sum().sort_values(ascending=False)
             st.bar_chart(ingresos_por_categoria)
 
-            # 11. Filtros (opcional)
+            # 10. Filtros (opcional)
             st.sidebar.header('Filtros')
             fecha_inicio = st.sidebar.date_input('Fecha Inicio', df['Fecha'].min())
             fecha_fin = st.sidebar.date_input('Fecha Fin', df['Fecha'].max())
